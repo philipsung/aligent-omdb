d5e73cf9267eec4e2ad5cadd57ad50be0b457dfc
Philip Sung <philip.f.sung@gmail.com>
2021-04-17 23:18:12 +0930

    queryAPI function added to searchHeader. Full list generating successfully

--- src/searchHeader.js
+++ src/searchHeader.js
@@ -5,16 +5,28 @@ import 'rc-slider/assets/index.css';
 export default function SearchHeader() {
 
 	//search states
-	const [query, setQuery] = useState('')
-	const [movies, setMovies] = useState([])
-	const [sliderValues, setSliderValues] = useState([])
+	const [queryText, setQueryText] = useState('')
+	const [queryType, setQueryType] = useState('')
+	const [queryYear, setqueryYear] = useState([1970,2015])
 
 	const queryAPI = async(e) => {
 		//prevent page reload on form submission
 		e.preventDefault();	
-		console.log(sliderValues)
-		console.log(e.target)
-		const url = 'http://www.omdbapi.com/?apikey=19bc8d19&${query}'
+
+		let movies = []
+
+		//API only allows searching by individual years -> repeat for range
+		for (let i = queryYear[0]; i <= queryYear[1]; i++) {
+			const url = `http://www.omdbapi.com/?apikey=19bc8d19&s=${queryText}&y=${i}&type=${queryType}`
+			try {
+				let res = await fetch(url)
+				let data = await res.json()
+				movies = movies.concat(data.Search)
+				console.log(`${data.Search.length} results for ${i}. Total ${movies.length} results`)
+			} catch(err) {
+				console.error(err)
+			}
+		}
 	}
 
 	const currentYear = new Date().getFullYear()
@@ -26,27 +38,29 @@ export default function SearchHeader() {
 				<input className="input" type="text" 
 					name="queryText"
 					placeholder="E.g. Parasite"
+					onChange={ (e) => setQueryText(e.target.value)}
 				/>
 			</div>
 			<div id="form-parameters">
 				<div id="form-parameters-left">
 					<label className="label" htmlFor="queryYear">YEAR</label>
-					<span id="minimumYear">Minimum</span>
-					<Range id="slider" min={1880} max={currentYear}
-						defaultValue={[1970,2015]}
-						allowCross={false}
-						onChange={(newValues) => setSliderValues(newValues)}
-						
-					/>
-					<span id="maximumYear">Maximum</span>
+					<div id="form-slider">
+						<span id="minimumYear">{queryYear[0]}</span>
+						<Range id="slider" min={1880} max={currentYear}
+							defaultValue={[1970,2015]}
+							allowCross={false}
+							onChange={(newValues) => setqueryYear(newValues)}
+						/>
+						<span id="maximumYear">{queryYear[1]}</span>
+					</div>
 				</div>
 				
-				<div id="form-parameters-right">
+				<div id="form-parameters-right" onChange={ (e) => setQueryType(e.target.value)}>
 					<label className="label" htmlFor="queryType">TYPE</label>
-					<input type="radio" value="" name="queryType" />Any
-					<input type="radio" value="Movies" name="queryType" />Movies
-					<input type="radio" value="Series" name="queryType" />Series
-					<input type="radio" value="Episodes" name="queryType" />Episodes
+					<input type="radio" value="" name="queryType" defaultChecked />Any
+					<input type="radio" value="movies" name="queryType" />Movies
+					<input type="radio" value="series" name="queryType" />Series
+					<input type="radio" value="episodes" name="queryType" />Episodes
 				</div>
 			</div>
 		</form>

1f11ac410cf4c3ffe2e7851bb70d4eb5dcbf58fc
Philip Sung <philip.f.sung@gmail.com>
2021-04-18 10:31:01 +0930

    searchResults moved into searchHeader for testing. Movies array changed from var to state

--- src/searchHeader.js
+++ src/searchHeader.js
@@ -8,12 +8,15 @@ export default function SearchHeader() {
 	const [queryText, setQueryText] = useState('')
 	const [queryType, setQueryType] = useState('')
 	const [queryYear, setqueryYear] = useState([1970,2015])
+	const [movies, setMovies] = useState([])
+	const currentYear = new Date().getFullYear()
 
 	const queryAPI = async(e) => {
 		//prevent page reload on form submission
 		e.preventDefault();	
 
-		let movies = []
+		//Clear any previous results
+		setMovies([])
 
 		//API only allows searching by individual years -> repeat for range
 		for (let i = queryYear[0]; i <= queryYear[1]; i++) {
@@ -21,7 +24,10 @@ export default function SearchHeader() {
 			try {
 				let res = await fetch(url)
 				let data = await res.json()
-				movies = movies.concat(data.Search)
+
+				setMovies([0,1,2])
+				console.log(url)
+				console.log(data.Search)
 				console.log(`${data.Search.length} results for ${i}. Total ${movies.length} results`)
 			} catch(err) {
 				console.error(err)
@@ -29,40 +35,53 @@ export default function SearchHeader() {
 		}
 	}
 
-	const currentYear = new Date().getFullYear()
-
+	
 	return (
-		<form className="form" onSubmit={queryAPI}>
-			<div id="form-text">
-				<label className="label" htmlFor="queryText">Search for:</label>
-				<input className="input" type="text" 
-					name="queryText"
-					placeholder="E.g. Parasite"
-					onChange={ (e) => setQueryText(e.target.value)}
-				/>
-			</div>
-			<div id="form-parameters">
-				<div id="form-parameters-left">
-					<label className="label" htmlFor="queryYear">YEAR</label>
-					<div id="form-slider">
-						<span id="minimumYear">{queryYear[0]}</span>
-						<Range id="slider" min={1880} max={currentYear}
-							defaultValue={[1970,2015]}
-							allowCross={false}
-							onChange={(newValues) => setqueryYear(newValues)}
-						/>
-						<span id="maximumYear">{queryYear[1]}</span>
+		<>
+			<form className="form" onSubmit={queryAPI}>
+				<div id="form-text">
+					<label className="label" htmlFor="queryText">Search for:</label>
+					<input className="input" type="text" 
+						name="queryText"
+						placeholder="E.g. Parasite"
+						onChange={ (e) => setQueryText(e.target.value)}
+					/>
+				</div>
+
+				<div id="form-parameters">
+					<div id="form-parameters-left">
+						<label className="label" htmlFor="queryYear">YEAR</label>
+						<div id="form-slider">
+							<span id="minimumYear">{queryYear[0]}</span>
+							<Range id="slider" min={1880} max={currentYear}
+								defaultValue={[1970,2015]}
+								allowCross={false}
+								onChange={(newValues) => setqueryYear(newValues)}
+							/>
+							<span id="maximumYear">{queryYear[1]}</span>
+						</div>
+					</div>
+					
+					<div id="form-parameters-right" onChange={ (e) => setQueryType(e.target.value)}>
+						<label className="label" htmlFor="queryType">TYPE</label>
+						<input type="radio" value="" name="queryType" defaultChecked />Any
+						<input type="radio" value="movies" name="queryType" />Movies
+						<input type="radio" value="series" name="queryType" />Series
+						<input type="radio" value="episodes" name="queryType" />Episodes
 					</div>
 				</div>
-				
-				<div id="form-parameters-right" onChange={ (e) => setQueryType(e.target.value)}>
-					<label className="label" htmlFor="queryType">TYPE</label>
-					<input type="radio" value="" name="queryType" defaultChecked />Any
-					<input type="radio" value="movies" name="queryType" />Movies
-					<input type="radio" value="series" name="queryType" />Series
-					<input type="radio" value="episodes" name="queryType" />Episodes
+			</form>
+
+			<div id="search-results">
+				<div id="search-results-list">
+					console.log(movies)
+				</div>
+
+				<div id="search-results-details">
+				<h2>Movie Title</h2>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 				</div>
 			</div>
-		</form>
+		</>
 	)
 }
--- src/searchResults.js
+++ src/searchResults.js
@@ -3,22 +3,7 @@ import React, {useState} from "react";
 export default function SearchResults() {
 
 	return (
-		<div id="search-results">
-			<div id="search-results-list">
-				<ul>
-					<li>Item 1</li>
-					<li>Item 2</li>
-					<li>Item 3</li>
-					<li>Item 4</li>
-					<li>Item 5</li>
-					<li>Item 6</li>
-				</ul>
-			</div>
-
-			<div id="search-results-details">
-			<h2>Movie Title</h2>
-			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-			</div>
-		</div>
+		<>
+		</>
 	)
 }
